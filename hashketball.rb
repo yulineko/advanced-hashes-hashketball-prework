657e1e640a6d2211d5fd099cbabc8cca2397ac73
Yuliya Daroshka <yuliya.daroshka@gmail.com>
2019-07-09 17:23:39 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,143 @@
 # Write your code here!
 
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black, White"],
+      :players => [
+        {
+          :name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assist => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1
+        },
+        {
+          :name => "Reggie Evans",
+          :number => 30,
+          :shoe => 14,
+          :points => 12,
+          :rebounds => 12,
+          :assist => 12,
+          :steals => 12,
+          :blocks =>12,
+          :slam_dunks => 7
+        },
+        {
+          :name => "Brook Lopez",
+          :number => 11,
+          :shoe => 17,
+          :points => 17,
+          :rebounds => 19,
+          :assist => 10,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 15
+        },
+        {
+          :name => "Mason Pumlee",
+          :number => 1,
+          :shoe => 19,
+          :points => 26,
+          :rebounds => 11,
+          :assist => 6,
+          :steals => 3,
+          :blocks => 8,
+          :slam_dunks => 5
+        },
+        {
+          :name => "Jason Terry",
+          :number => 31,
+          :shoe => 15,
+          :points => 19,
+          :rebounds => 2,
+          :assist => 2,
+          :steals => 4,
+          :blocks => 11,
+          :slam_dunks => 1
+        }
+        ]
+    },
+    
+    :away => {
+      :team_name => "Charlotter Hornets",
+      :colors  => ["Turquoise, Purple"],
+      :players  => [
+        {
+          :name => "Jeff Adrien",
+          :number => 4,
+          :shoe => 18,
+          :points => 10,
+          :rebounds => 1,
+          :assist => 1,
+          :steals => 2,
+          :blocks => 7,
+          :slam_dunks => 2
+        },
+        {
+          :name => "Bismack Biyombo",
+          :number => 0,
+          :shoe => 16,
+          :points => 12,
+          :rebounds => 4,
+          :assist => 7,
+          :steals => 22,
+          :blocks => 15,
+          :slam_dunks => 10
+        },
+        {
+          :name => "DeSagna Diop",
+          :number => 2,
+          :shoe => 14,
+          :points => 24,
+          :rebounds => 12,
+          :assist => 12,
+          :steals => 4,
+          :blocks => 5,
+          :slam_dunks => 5
+        },
+        {
+          :name => "Ben Gordon",
+          :number => 8,
+          :shoe => 15,
+          :points => 33,
+          :rebounds => 3,
+          :assist => 2,
+          :steals => 1,
+          :blocks => 1,
+          :slam_dunks => 0
+        }
+        {
+          :name => "Kemba Walker",
+          :number => 33,
+          :shoe => 15,
+          :points => 6,
+          :rebounds => 12,
+          :assist => 12,
+          :steals => 7,
+          :blocks => 5,
+          :slam_dunks => 12
+        }
+        ]
+    }    
+  }
+end
 
-
-
+def num_points_scored(name) 
+  game = game_hash
+  game.each do |team, team_attibutes|
+    team_attibutes[:players].each do |player, player_attributes|
+      if player_attributes[:name] == name
+        return player_attributes[:players][:points]
+      end
+    end
+  end
+end
 
 
 
